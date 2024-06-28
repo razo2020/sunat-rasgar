dac2ee74c202188ce9a7bc2c0409e1d6ffc191bc
LUIS ALVARADO <wow.trujillo@gmail.com>
2024-06-28 17:26:15 -0500

    se actualiza la descarga de sunat masivo

--- print.py
+++ print.py
@@ -4,7 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.remote import webelement
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
 import re
 import time
@@ -17,7 +18,7 @@ import click
 class shadowDOM :
     def __init__(self, driver:webdriver.Chrome) :
         self.driver = driver
-        self.root:webelement.WebElement
+        self.root:WebElement
     
     #función recursiva
     def CSS(self, css:str):
@@ -33,13 +34,13 @@ class shadowDOM :
             arguments[0](null)""")
 
     def Tag(self, element:str):
-        self.root = self.driver.execute_script('return arguments[0].shadowRoot',self.root.find_element(By.TAG_NAME, element))
+        self.root = self.driver.execute_script('return arguments[0].shadowRoot',self.driver.find_element(By.TAG_NAME, element))
 
-    #해당 Element 클릭
-    def CSSck (self):
+    #dando click al Element
+    def CSSck(self):
         self.root.click()
         text = self.root.text
-        print(text+" has echo click")
+        print(f"{text} has echo click")
 
 def imprimir(driver:webdriver.Chrome, src:str):
     src = re.sub("[()]*", "", src, flags=re.IGNORECASE)
@@ -47,29 +48,24 @@ def imprimir(driver:webdriver.Chrome, src:str):
     print(src)
     if os.path.isfile(src):
         os.remove(src)
-    #ventana de imprimir
+    #ventana de imprimir 
     original_window = driver.current_window_handle
-
     pyautogui.hotkey('ctrl', 'p')
     time.sleep(1)
     driver.switch_to.window(driver.window_handles[-1])
-    time.sleep(1)
 
-    pdf = shadowDOM(driver)
-    pdf.Tag('print-preview-app')
-    pdf.CSS('#sidebar')
-    pdf.CSS('#destinationSettings')
-    pdf.CSS('#destinationSelect')
-    pdf.CSS('print-preview-settings-section:nth-child(9) > div > select > option:nth-child(3)[value="Save as PDF/local/"]')
-    pdf.CSSck()
+    _extracted_from_imprimir(
+        driver
+        , '#destinationSettings'
+        , '#destinationSelect'
+        , 'print-preview-settings-section:nth-child(9) > div > select > option:nth-child(3)[value="Save as PDF/local/"]'
+    )
     time.sleep(1)
-
-    SaveCK = shadowDOM(driver)
-    SaveCK.Tag('print-preview-app')
-    SaveCK.CSS('#sidebar')
-    SaveCK.CSS('print-preview-button-strip')
-    SaveCK.CSS('div > cr-button.action-button')
-    SaveCK.CSSck()
+    _extracted_from_imprimir(
+        driver
+        , 'print-preview-button-strip'
+        , 'div > cr-button.action-button'
+    )
     time.sleep(1)
 
     app = Application().connect(class_name="#32770", title='Guardar como')
@@ -78,13 +74,17 @@ def imprimir(driver:webdriver.Chrome, src:str):
     app.Guardar.Button1.click()
     driver.switch_to.window(original_window)
 
+
+# TODO Rename this here and in `imprimir`
+def _extracted_from_imprimir(driver, *arg):
+    result = shadowDOM(driver)
+    result.Tag('print-preview-app')
+    result.CSS('#sidebar')
+    for a in arg:
+        result.CSS(a)
+    result.CSSck()
+
 def buscar(ruc, destino, prefijo):
-    
-    if str.find(destino,":") == -1 :
-        actual = os.path.dirname(__file__)
-        destino = os.path.join(actual,destino)
-    if not os.path.exists(destino):
-        os.makedirs(destino)
     url = "https://e-consultaruc.sunat.gob.pe/cl-ti-itmrconsruc/FrameCriterioBusquedaWeb.jsp"
     chrome_service = Service(ChromeDriverManager().install())
     #chrome_service = Service(executable_path=r"C:/Users/Lenovo/Downloads/recuperar/web/chromedriver-win64/chromedriver.exe")
@@ -93,58 +93,64 @@ def buscar(ruc, destino, prefijo):
     options.add_argument('--ignore-ssl-errors')
     #options.add_argument('--headless=new')
     driver = webdriver.Chrome(service=chrome_service,options=options)
-    driver.implicitly_wait(90)
+    driver.implicitly_wait(20)
     driver.get(url)
     #driver.maximize_window()
-    #time.sleep(1)
+    
+    if str.find(destino,":") == -1 :
+        actual = os.path.dirname(__file__)
+        destino = os.path.join(actual,destino)
+    if not os.path.exists(destino):
+        os.makedirs(destino)
+    
     elem = driver.find_element(By.ID, 'txtRuc')
     elem.clear()
     elem.send_keys(ruc)
-    wiat = WebDriverWait(driver, 10)
+    wiat = WebDriverWait(driver, 25)
     elem = wiat.until(EC.presence_of_element_located((By.ID, 'btnAceptar')))
     elem.click()
-    time.sleep(1)
     destino = destino+prefijo+ruc+"_"
-    #Consulta RUC
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[1]/h1')))
-    src = destino+elem.text+".pdf"
-    imprimir(driver,src)
-    #Información Histórica
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[1]/div[1]/form/button')))
-    src = destino+elem.text+".pdf"
-    elem.click()
-    time.sleep(1)
-    imprimir(driver,src)
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/button[1]')))
-    elem.click()
-    #Cantidad de Trabajadores y/o Prestadores de Servicio
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[2]/div[1]/form/button')))
-    src = destino+elem.text.replace("/","_")+".pdf"
-    elem.click()
-    time.sleep(1)
-    imprimir(driver,src)
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[4]/button[1]')))
-    elem.click()
-    #Representante(s) Legal(es)
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[3]/div[3]/form/button')))
-    src = destino+elem.text+".pdf"
-    elem.click()
-    time.sleep(1)
-    imprimir(driver,src)
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/button[1]')))
-    elem.click()
-    #Establecimiento(s) Anexo(s)
-    elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[4]/div/form/button')))
-    if elem.size != 0:
-        src = destino+elem.text+".pdf" 
+    
+    try:
+        #Consulta RUC
+        elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[1]/h1')))
+        imprimir(driver,destino+elem.text+".pdf")
+
+        #Información Histórica
+        elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[1]/div[1]/form/button')))
+        src=destino+elem.text+".pdf"
         elem.click()
-        time.sleep(1)
         imprimir(driver,src)
-        elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[3]/button[1]')))
+        elem = wiat.until(EC.presence_of_element_located((By.CLASS_NAME, 'btnNuevaConsulta')))
         elem.click()
 
-    driver.quit
-    driver.close
+        #Cantidad de Trabajadores y/o Prestadores de Servicio
+        elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[2]/div[1]/form/button')))
+        src=destino+elem.text.replace("/","_")+".pdf"
+        elem.click()
+        imprimir(driver,src)
+        elem = wiat.until(EC.presence_of_element_located((By.CLASS_NAME, 'btnNuevaConsulta')))
+        elem.click()
+
+        #Representante(s) Legal(es)
+        elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[3]/div[3]/form/button')))
+        src=destino+elem.text+".pdf"
+        elem.click()
+        imprimir(driver,src)
+        elem = wiat.until(EC.presence_of_element_located((By.CLASS_NAME, 'btnNuevaConsulta')))
+        elem.click()
+
+        #Establecimiento(s) Anexo(s)
+        elem = wiat.until(EC.presence_of_element_located((By.XPATH, '/html/body/div/div[2]/div/div[5]/div[4]/div/form/button')))
+        src=destino+elem.text+".pdf"
+        elem.click()
+        imprimir(driver,src)
+        elem = wiat.until(EC.presence_of_element_located((By.CLASS_NAME, 'btnNuevaConsulta')))
+        elem.click()
+
+    except TimeoutException as ex:
+        driver.quit
+        driver.close
 
 @click.command(name='print')
 @click.option('--destino', '-d', default='dest\\empresa10\\', help='Destination for final pdf file')
